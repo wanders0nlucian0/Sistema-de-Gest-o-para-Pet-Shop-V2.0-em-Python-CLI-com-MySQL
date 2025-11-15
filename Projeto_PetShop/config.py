2f725eca469a792096b5c334eb7f3cb4fd9a62d8
wanders0nlucian0o <antonio56578676@alunos.ce.senac.br m>
2025-11-15 02:06:20 -0300

    Adiciona .gitignore e configura uso de .env para credenciais

--- Projeto_PetShop/config.py
+++ Projeto_PetShop/config.py
@@ -1,22 +1,22 @@
 import os
+from dotenv import load_dotenv
 
-# ======================================================================
-# CONFIGURAÇÕES GERAIS
-# ======================================================================
+# Carrega o .env
+load_dotenv()
 
-PASTA = "NOME DA PASTA DO BANCO DE DADOS"
+PASTA = os.getenv("PASTA_DADOS", "dados_petshop")
 os.makedirs(PASTA, exist_ok=True)
 
 # CONFIGURAÇÕES DE E-MAIL
-EMAIL_REMETENTE = "SEU E-MAIL"
-SENHA_APP_REMETENTE = "SUA SENHA"  # <<< TROCAR PELA SENHA DE APP DO GMAIL
+EMAIL_REMETENTE = os.getenv("EMAIL_REMETENTE")
+SENHA_APP_REMETENTE = os.getenv("SENHA_APP_REMETENTE")
 
 # CONFIGURAÇÕES DO BOT DO TELEGRAM
-BOT_TOKEN = "TOKEN DO TELEGRAM"  # <<< TROCAR PELO TOKEN DO BOT
-CHAT_ID = "ID DO TELEGRAM"  # <<< TROCAR PELO CHAT ID
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 # CONFIGURAÇÕES DO MYSQL
-MYSQL_HOST = "LOCAL DO SEU BANCOS DE DADOS"
-MYSQL_USER = "USER DO BANCO DE DADOS"
-MYSQL_PASSWORD = "SENHA DO BANCO DE DADOS"  # <<< TROCAR PELA SENHA DO MYSQL
-MYSQL_DB = "NOME DO ARQUIVO DO BANCO DE DADOS"
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB", "meu_querido_pet")
